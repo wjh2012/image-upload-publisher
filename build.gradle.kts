2d07ecb5d6b39baed2578d1f7d623fae6cc146ed
wjh <wjh2012@naver.com>
2025-02-19 23:16:31 +0900

    dependency: postgres driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
