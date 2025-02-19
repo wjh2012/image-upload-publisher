11379cbbdc5a0b3759a8fa49dd483a38310e5b57
wjh <wjh2012@naver.com>
2025-02-19 23:16:51 +0900

    feat: SKIP LOCKED

--- src/main/kotlin/ggomg/imageuploadpublisher/outbox/repository/OutboxRepository.kt
+++ src/main/kotlin/ggomg/imageuploadpublisher/outbox/repository/OutboxRepository.kt
@@ -2,5 +2,15 @@ package ggomg.imageuploadpublisher.outbox.repository
 
 import ggomg.imageuploadpublisher.outbox.entity.OutboxEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
-interface OutboxRepository : JpaRepository<OutboxEntity, Long>, OutboxRepositoryCustom
+@Repository
+interface OutboxRepository : JpaRepository<OutboxEntity, Long>, OutboxRepositoryCustom {
+
+    @Query(
+        value = "SELECT * FROM outbox_entity WHERE status = 'PENDING' ORDER BY id LIMIT 10 FOR UPDATE SKIP LOCKED",
+        nativeQuery = true
+    )
+    fun findTop10PendingQuery(): List<OutboxEntity>
+}
